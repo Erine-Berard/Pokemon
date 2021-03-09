e920e6d028a45e0be6f0f7e6a6b2756cb219ddb4
Erine-Berard <berard.erine@gmail.com>
2021-03-09 08:06:10 +0100

    Ajout 09/05

--- pokemon-Antoine-Erine/Jeu.cpp
+++ pokemon-Antoine-Erine/Jeu.cpp
@@ -7,6 +7,18 @@ using namespace std;
 
 void Jeu::Jouer(){
 
+	//creation des pokemons
+
+	vector <Pokemon> pokemons;
+	vector <string> type1;
+	type1.push_back("Plante");
+
+	vector <string> type2;
+	type2.push_back("Feu");
+
+	vector <string> type3;
+	type3.push_back("Eau");
+
 	vector <Attaques> attaques1;
 	Attaques Charge("Charge", "Normal", 0, 1, 5, 4);
 	Attaques TranchHerbe("TranchHerbe", "Plante", 1, 1, 10, 4);
@@ -61,93 +73,59 @@ void Jeu::Jouer(){
 	Attaques PistoletaO("PistoletaO", "Eau", 1, 1, 10, 4);
 	Attaques Charge("Charge", "Normal", 0, 1, 5, 4);
 
-	vector <string> type1;
-	type1.push_back("Poison");
-	type1.push_back("Plante");
 	attaques1.push_back(Charge);
 	attaques1.push_back(TranchHerbe);
 	attaques1.push_back(FouetLianes);
 	attaques1.push_back(Belier);
 	Pokemon Bulbizarre("Bulbizarre", 300, type1, 45, 1, 49, 65, 49, 65, 45, attaques1, );
 
-	vector <string> type2;
-	type2.push_back("Feu");
-	vector <Attaques> attaques2;
 	attaques2.push_back(Griffe);
 	attaques2.push_back(CrocsFeu);
 	attaques2.push_back(Flammeche);
 	attaques2.push_back(FeudEnfer);
 	Pokemon Salameche("Salameche", 300, type2, 39, 1, 52, 60, 43, 60, 65, attaques2, );
 
-
-	vector <string> type3;
-	type3.push_back("Eau");
-	vector <Attaques> attaques3;
 	attaques3.push_back(Charge);
 	attaques3.push_back(Ecume);
 	attaques3.push_back(PistoletaO);
 	attaques3.push_back(Hydrocanon);
 	Pokemon Carapuce("Carapuce", 300, type3, 44, 1, 48, 50, 65, 64, 43, attaques3, );
 
-
-	vector <string> type4;
-	type4.push_back("Plante");
-	vector <Attaques> attaques4;
 	attaques4.push_back(Charge);
 	attaques4.push_back(LanceSoleil);
 	attaques4.push_back(TranchHerbe);
 	attaques4.push_back(FeuilleMagik);
-	Pokemon Germignion("Germignion", 300, type4, 45, 1, 49, 59, 65, 65, 45, attaques4, );
+	Pokemon Germignion("Germignion", 300, type1, 45, 1, 49, 59, 65, 65, 45, attaques4, );
 
-
-	vector <string> type5;
-	type5.push_back("Feu");
-	vector <Attaques> attaques5;
 	attaques5.push_back(Charge);
 	attaques5.push_back(RouedeFeu);
 	attaques5.push_back(FlammeUltime);
 	attaques5.push_back(Flammeche);
-	Pokemon Hericendre("Hericendre", 300, type5, 39, 1, 52, 60, 43, 50, 65, attaques5, );
+	Pokemon Hericendre("Hericendre", 300, type2, 39, 1, 52, 60, 43, 50, 65, attaques5, );
 
-
-	vector <string> type6;
-	type6.push_back("Eau");
-	vector <Attaques> attaques6;
 	attaques6.push_back(Griffe);
 	attaques6.push_back(SuperPuissance);
 	attaques6.push_back(PistoletaO);
 	attaques6.push_back(Morsure);
-	Pokemon Kaiminus("Kaiminus", 300, type6, 50, 1, 65, 44, 64, 48, 43, attaques6, );
+	Pokemon Kaiminus("Kaiminus", 300, type3, 50, 1, 65, 44, 64, 48, 43, attaques6, );
 
-
-	vector <string> type7;
-	type7.push_back("Plante");
-	vector <Attaques> attaques7;
 	attaques7.push_back(EcrasFace);
 	attaques7.push_back(TempeteVerte);
 	attaques7.push_back(ViveAttaque);
 	attaques7.push_back(VoldeVie);
-	Pokemon Arcko("Arcko", 300, type7, 40, 1, 45, 64, 35, 55, 70, attaques7, );
+	Pokemon Arcko("Arcko", 300, type1, 40, 1, 45, 64, 35, 55, 70, attaques7, );
 
-
-	vector <string> type8;
-	type8.push_back("Feu");
-	vector <Attaques> attaques8;
 	attaques8.push_back(Griffe);
 	attaques8.push_back(BouteFeu);
 	attaques8.push_back(ViveAttaque);
 	attaques8.push_back(Flammeche);
-	Pokemon Poussifeu("Poussifeu", 300, type8, 45, 1, 60, 70, 40, 50, 45, attaques8, );
+	Pokemon Poussifeu("Poussifeu", 300, type2, 45, 1, 60, 70, 40, 50, 45, attaques8, );
 
-
-	vector <string> type9;
-	type9.push_back("Eau");
-	vector <Attaques> attaques9;
 	attaques9.push_back(Charge);
 	attaques9.push_back(Hydrocanon);
 	attaques9.push_back(PistoletaO);
 	attaques9.push_back(CoudBoue);
-	Pokemon Gobou("Gobou", 300, type9, 50, 1, 70, 50, 50, 50, 40, attaques9, );
+	Pokemon Gobou("Gobou", 300, type3, 50, 1, 70, 50, 50, 50, 40, attaques9, );
 
 	// creation vecteur avec tout les pokemons
 	vector <Pokemon> pokemons;
